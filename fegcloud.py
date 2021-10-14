4ff00368a811914ac340d15a547fd4ac8efcb201
FireFlashing <filippoettoreg@gmail.com>
2021-10-14 08:38:08 +0200

    First tui attempts

--- fegcloud.py
+++ fegcloud.py
@@ -9,6 +9,7 @@ from rich.console import Console
 from rich.table import Table
 from rich.box import SQUARE
 from pynput import keyboard
+from math import ceil, floor
 
 current_path = os.path.dirname(os.path.abspath(__file__))+"/"
 logging.basicConfig(filename=current_path+"latest.log",filemode='a',format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',datefmt='%H_%M_%S',level=logging.DEBUG)
@@ -154,13 +155,117 @@ class RandomBot():
         console.print("[green]Starting polling[/green]")
         self.bottino.polling()
         console.print("[green]Ended polling[/green]")
+class IndexHelper():
+    """Cool and good class to help with the pretty not cool indexing of things"""
+    def __init__(self,dictionary):
+        self.current_path=["root"]
+        self.previous_path="root"
+        self.dictionary=dictionary
+    #TODO: add method to know maximum page amount
+    def get_current_content(self):
+        """Get the content of the current path"""
+        return self.get_dict_from_path(self.current_path)
+    def get_dict_from_path(self,path_list_input):
+        """Get the dictionary for the given path list"""
+        path_list=path_list_input.copy()
+        this_dict=self.dictionary
+        while len(path_list)>0:
+            if path_list[0] in this_dict:
+                if this_dict[path_list[0]]["type"]=="folder":
+                    this_dict=this_dict[path_list[0]]["content"]
+            del[path_list[0]]
+        return this_dict
+    def get_max_page(self,page,size_y,path=[]):
+        """Gets how many pages can be in this dict; returns a dictionary which looks like this: {"max_pages":1.825,"floor":1,"ceil":2}"""
+        output_dict={}
+        
+        if path==[]:
+            path=self.current_path
+        size_y-=6
+        total_elements=self.get_items_in_dict(path)
+
+        max_pages=total_elements/size_y
+        output_dict["max_pages"]=max_pages
+
+        output_dict["floor"]=floor(max_pages)
+        output_dict["ceil"]=ceil(max_pages)
+
+        return output_dict
+
+    def get_items_in_dict(self,path=[]):
+        """Gets amount of items in said path"""
+        if path==[]:
+            path=self.current_path
+        current_dict=self.get_dict_from_path(path)
+        i=0
+        for element in current_dict:
+            i+=1
+        return i
+    def get_items_in_page(self,size_y,page,path=[]):
+        """Gets the amount of items in a page in a said path; size_y must be with the 6, it should be the actual size of the terminal"""
+        if path==[]:
+            path=self.current_path
+        pages_amount=self.get_max_page(page,size_y,path)
+        if page<pages_amount["floor"]:
+            return size_y-6
+        else:
+            return self.get_items_in_dict(path)-((size_y-6)*pages_amount["floor"])
+
+
+class Cloud():
+    def __init__(self):
+        cloud_dict=jread("cloud")
+        self.y_size=os.get_terminal_size()[1]
+        self.index_helper=IndexHelper(cloud_dict)
+        self.page=0#Sets the current selection page to 0
+        self.cursor=0
+        self.display_table()
+    def create_table(self,data_dict={}):
+        output_table=Table(box=SQUARE)
+        output_table.add_column("Id",style="bold green")
+        output_table.add_column("Name")
+        i=0
+        for element in data_dict:
+            if self.y_size*self.page<=i+6<=(self.y_size*(self.page+1))-1:
+                if data_dict[element]["type"]=="folder":
+                    element_name=element+"/"
+                else:
+                    element_name=element
+                if i==self.cursor:
+                    output_table.add_row("[black on white]"+str(i)+"[/black on white]",element_name)
+                else:
+                    output_table.add_row(str(i),element_name)
+            i+=1
+        return output_table
+
+    def display_table(self):
+        while True:
+            console.clear()
+            console.print(self.create_table(self.index_helper.get_current_content()))
+            console.print("P%i"%(self.page))
+            items_in_page=self.index_helper.get_items_in_page(self.y_size,self.page)
+            pressed_key=KeyWaiter().wait_for_key()
+            if pressed_key=="Key.right":
+                if self.page+1<=self.index_helper.get_max_page(self.page,self.y_size)["floor"]:
+                    self.page+=1
+                    self.cursor+=(self.y_size-6)#TODO: should get a constant for this 6
+            elif pressed_key=="Key.left":
+                if self.page-1>=0:
+                    self.page-=1
+                    self.cursor-=(self.y_size-6)
+            elif pressed_key=="Key.down":
+                self.cursor+=1
+            elif pressed_key=="Key.up":
+                self.cursor-=1
 
 this_bot=RandomBot()
 while True:
     console.clear()
-    action_input=get_input("Which action would you like to perform?\n[bold green]0[/bold green]│start polling\n[bold green]1[/bold green]│change settings\n",["0","1"],print_not_valid_message=False)
+    action_input=get_input("Which action would you like to perform?\n[bold green]0[/bold green]│start polling\n[bold green]1[/bold green]│change settings\n[bold green]2[/bold green]│use the drive\n",["0","1","2"],print_not_valid_message=False)
     if action_input=="0":
         this_bot.start_polling()#Ok so it stops here while polling
         input("Press enter to continue using the program (terminal will be cleared)")
     elif action_input=="1":
         change_settings()
+    elif action_input=="2":
+        cloud=Cloud()
